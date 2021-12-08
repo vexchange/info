c1f77da16cb8a098a4f292f54f29debd1afee16f
hochbrj <jhochbrueckner@gmail.com>
2021-12-08 15:44:02 +1100

    use production api

--- utils/constants/vexchange.js
+++ utils/constants/vexchange.js
@@ -1,2 +1,2 @@
 export const FACTORY_ADDRESS = "0xB312582C023Cc4938CF0faEA2fd609b46D7509A2";
-export const API_BASE_URL = "http://127.0.0.1:58791/v1/";
+export const API_BASE_URL = "https://api.vexchange.io/v1/";
